f0afdfbd249f9b242602e6426f88f914ed3ac288
Sithembiso2002 <maissel49@gmail.com>
2025-12-04 20:17:18 +0200

    Update AdminDashboard.jsx

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import '../assets/css/ind.css';
 import '../assets/css/admin.css';
-import '../assets/css/adminhome.css';
+import '../assets/css/adminhome.css';ss
 import { Link } from "react-router-dom";
 import { 
   Users, 
