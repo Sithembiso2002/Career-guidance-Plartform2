15d6e636b49bd450e3b7801d16c13e55aeb00ecd
Sithembiso2002 <maissel49@gmail.com>
2025-12-04 20:23:30 +0200

    Update api.js

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'https://career-guidance-plartform-1.onrender.com'
+  baseURL: import.meta.env.VITE_API_URL || 'https://careerapp-final.onrender.com/'
                                             
   
   //baseURL: import.meta.env.VITE_API_URL || 'https://career-plartform-backend.onrender.com'
